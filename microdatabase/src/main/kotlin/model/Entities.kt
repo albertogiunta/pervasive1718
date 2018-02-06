9e6f02055b01c48d18979c4fb5950a5a67187384
albertogiunta <alb.giunta@gmail.com>
2018-02-06 10:46:55 +0100

    CHANGED deserialization in post requrests to be with json instead of queryParams

--- microdatabase/src/main/kotlin/model/Entities.kt
+++ microdatabase/src/main/kotlin/model/Entities.kt
@@ -3,20 +3,20 @@ package model
 import java.sql.Timestamp
 import java.util.*
 
-data class Activity(val id: Int, val name: String, val activityTypeId: Int, val acronym: String, val boundaryId: Int)
+data class Activity(val id: Int = 0, val name: String, val activityTypeId: Int, val acronym: String, val boundaryId: Int)
 
-data class ActivityType(val id: Int, val name: String) // farmaci, manovre, diagnostiche
+data class ActivityType(val id: Int = 0, val name: String) // farmaci, manovre, diagnostiche
 
-data class HealthParameter(val id: Int, val name: String, val acronym: String)
+data class HealthParameter(val id: Int = 0, val name: String, val acronym: String)
 
 data class Log(val id: Int = 0, val name: String, val logTime: Timestamp = Timestamp(Date().time), val healthParameterId: Int, val healthParameterValue: Double)
 
-data class Operator(val id: Int, val name: String, val surname: String, val roleId: Int, val isActive: Boolean)
+data class Operator(val id: Int = 0, val name: String, val surname: String, val roleId: Int, val isActive: Boolean)
 
-data class Role(val id: Int, val name: String) // leader, collaboratore, anestesista
+data class Role(val id: Int = 0, val name: String) // leader, collaboratore, anestesista
 
-data class Boundary(val id: Int, val healthParameterId: Int, val activityId: Int, val upperBound: Double, val lowerBound: Double)
+data class Boundary(val id: Int = 0, val healthParameterId: Int, val activityId: Int, val upperBound: Double, val lowerBound: Double)
 
-data class Task(val id: Int, val operatorId: Int, val startTime: Timestamp, val endTime: Timestamp, val activityId: Int, val taskStatusId: String)
+data class Task(val id: Int = 0, val operatorId: Int, val startTime: Timestamp, val endTime: Timestamp, val activityId: Int, val taskStatusId: String)
 
-data class TaskStatus(val id: Int, val name: String) // sospseso, in corso, terminato, eliminato
+data class TaskStatus(val id: Int = 0, val name: String) // sospseso, in corso, terminato, eliminato
