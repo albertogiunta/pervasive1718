46da8036954e2db5f5ba67e2da15182d58a136dd
Xander_ <alessandro.cevoli2@studio.unibo.it>
2018-02-07 16:32:15 +0100

    Completed MicroService-Notifier AMQP Client Wrapper, Controller and Reactive Adapter.
    TO DO: WS Management, health Parameters Analytics (see: Rx Observable Merge)

--- micronotifier/src/main/kotlin/amqp/client.kt
+++ micronotifier/src/main/kotlin/amqp/client.kt
@@ -6,10 +6,9 @@ import LifeParameters
 import core.NotifierController
 import RabbitMQSubscriber
 import BrokerConnector
-import com.rabbitmq.client.AMQP
-import com.rabbitmq.client.Consumer
-import com.rabbitmq.client.DefaultConsumer
-import com.rabbitmq.client.Envelope
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  *
@@ -18,26 +17,18 @@ import com.rabbitmq.client.Envelope
 class AMQPClient(val broker: BrokerConnector,
                  val controller: NotifierController<LifeParameters, Member, Session>) {
 
-    val amqpClient = RabbitMQSubscriber(broker)
-
-    val handlersMap = HashMap<LifeParameters, Consumer>()
+    val amqpSubscriber = RabbitMQSubscriber(broker)
+    val publishSubjects = ConcurrentHashMap<LifeParameters, Subject<String>>()
 
     init {
         with(controller) {
-            controller.topics().forEach {
-                amqpClient.subscribe(it, object : DefaultConsumer(broker.channel) {
-
-                    @Throws(java.io.IOException::class)
-                    override fun handleDelivery(consumerTag: String,
-                                                envelope: Envelope,
-                                                properties: AMQP.BasicProperties,
-                                                body: ByteArray) {
-                        val message = String(body, Charsets.UTF_8)
-                        println(message)
-                    }
+            controller.topics().forEach { lp ->
+                publishSubjects[lp] = PublishSubject.create<String>()
+                //publishSubjects[lp]?.subscribe(Logger::info)
+                amqpSubscriber.subscribe(lp, amqpSubscriber.createStringConsumer {
+                    publishSubjects[lp]?.onNext(it)
                 })
             }
         }
     }
-
 }
--- micronotifier/src/main/kotlin/core/controller.kt
+++ micronotifier/src/main/kotlin/core/controller.kt
@@ -4,20 +4,27 @@ import logic.Member
 import spark.Session
 import java.util.concurrent.ConcurrentHashMap
 import LifeParameters
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
+import java.util.HashMap
 
 interface NotifierController<T, L, S> {
 
-    fun topics(): Set<T>
-
     fun setSessionFor(listener: L, session: S)
 
-    fun removeListener(listener: L)
+    fun getSessionOf(listener: L): Session?
 
     fun removeSession(session: S)
 
     fun addListenerTo(topic: T, listener: L)
 
+    fun getListenersOn(topic: T): Set<L>?
+
+    fun removeListener(listener: L)
+
     fun addTopic(topic: T)
+
+    fun topics(): Set<T>
 
     fun addTopics(topics: Set<T>)
 
@@ -30,17 +37,11 @@ class NotifierControllerImpl(private var topics: Set<LifeParameters>) : Notifier
     val lifeParametersMap = ConcurrentHashMap<LifeParameters, Set<Member>>()
     val sessionMap = ConcurrentHashMap<Member, Session>()
 
-    override fun topics(): Set<LifeParameters> {
-        return topics
-    }
-
     override fun setSessionFor(listener: Member, session: Session) {
         sessionMap[listener] = session
     }
 
-    override fun removeListener(listener: Member) {
-        sessionMap.remove(listener)
-    }
+    override fun getSessionOf(listener: Member): Session? = sessionMap[listener]
 
     override fun removeSession(session: Session) {
         sessionMap.keySet(session).forEach { sessionMap.remove(it) }
@@ -51,6 +52,12 @@ class NotifierControllerImpl(private var topics: Set<LifeParameters>) : Notifier
         lifeParametersMap.merge(topic, setOf(listener), { t, u -> t + u })
     }
 
+    override fun getListenersOn(topic: LifeParameters): Set<Member>? = lifeParametersMap[topic]
+
+    override fun removeListener(listener: Member) {
+        sessionMap.remove(listener)
+    }
+
     override fun addTopic(topic: LifeParameters) {
         topics += topic
     }
@@ -59,13 +66,19 @@ class NotifierControllerImpl(private var topics: Set<LifeParameters>) : Notifier
         this.topics += topics
     }
 
+    override fun topics(): Set<LifeParameters> = topics
+
     override fun removeTopic(topic: LifeParameters) {
         topics -= topic
         lifeParametersMap.remove(topic)
     }
 
     init {
-
+        with(topics) {
+            topics.forEach {
+                lifeParametersMap[it] = emptySet()
+            }
+        }
     }
 
 }
