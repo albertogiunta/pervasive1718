ed2e4c6fdd3b2c502fae0590b8f2984d560a858c
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-02-12 12:25:22 +0100

    ADDED the SessionController test in the micronotifier module

---
+++ micronotifier/src/test/kotlin/controller/NotifierSessionsControllerTest.kt
@@ -0,0 +1,244 @@
+package controller
+
+import logic.Member
+import org.eclipse.jetty.websocket.api.*
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.net.InetSocketAddress
+
+
+class NotifierSessionsControllerTest {
+
+    lateinit var sesCon: SessionsController<Member, Session>
+
+    val m1 = Member(888, "Mario Rossi")
+    val m2 = Member(777, "Padre Pio")
+
+    @Before
+    fun setUp() {
+        sesCon = NotifierSessionsController.singleton()
+    }
+
+    @Test
+    fun setAndGet() {
+        val ses = object : Session {
+            override fun getRemote(): RemoteEndpoint {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getLocalAddress(): InetSocketAddress {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun disconnect() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getProtocolVersion(): String {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getUpgradeResponse(): UpgradeResponse {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun setIdleTimeout(ms: Long) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getPolicy(): WebSocketPolicy {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getUpgradeRequest(): UpgradeRequest {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun suspend(): SuspendToken {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun isOpen(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getIdleTimeout(): Long {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close(closeStatus: CloseStatus?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close(statusCode: Int, reason: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun isSecure(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getRemoteAddress(): InetSocketAddress {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        }
+
+        sesCon.set(m1, ses)
+        assertEquals(sesCon.get(m1)!!, ses)
+    }
+
+
+    @Test
+    fun removeListener() {
+        val ses = object : Session {
+            override fun getRemote(): RemoteEndpoint {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getLocalAddress(): InetSocketAddress {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun disconnect() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getProtocolVersion(): String {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getUpgradeResponse(): UpgradeResponse {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun setIdleTimeout(ms: Long) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getPolicy(): WebSocketPolicy {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getUpgradeRequest(): UpgradeRequest {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun suspend(): SuspendToken {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun isOpen(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getIdleTimeout(): Long {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close(closeStatus: CloseStatus?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close(statusCode: Int, reason: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun isSecure(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getRemoteAddress(): InetSocketAddress {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        }
+
+        sesCon.set(m1, ses)
+        assertEquals(sesCon.removeListener(m1)!!, ses)
+        assertNull(sesCon.removeListener(m2))
+    }
+
+    @Test
+    fun removeListenerOn() {
+        val ses = object : Session {
+            override fun getRemote(): RemoteEndpoint {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getLocalAddress(): InetSocketAddress {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun disconnect() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getProtocolVersion(): String {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getUpgradeResponse(): UpgradeResponse {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun setIdleTimeout(ms: Long) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getPolicy(): WebSocketPolicy {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getUpgradeRequest(): UpgradeRequest {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun suspend(): SuspendToken {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun isOpen(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getIdleTimeout(): Long {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close(closeStatus: CloseStatus?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun close(statusCode: Int, reason: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun isSecure(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getRemoteAddress(): InetSocketAddress {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        }
+
+        sesCon.set(m1, ses)
+        val iter = sesCon.removeListenerOn(ses)
+        iter.forEach({ println(it) })
+        assertTrue(iter.contains(m1))
+        assertFalse(iter.contains(m2))
+    }
+
+}
