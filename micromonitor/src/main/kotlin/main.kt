f255a7e7b37186c613101972f505ab0cf76e9c72
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-02-12 10:06:31 +0100

    fix

--- micromonitor/src/main/kotlin/main.kt
+++ micromonitor/src/main/kotlin/main.kt
@@ -16,31 +16,31 @@ fun main(argv: Array<String>) {
 
     val obsTempGen = ObservableMonitor<Double>(tempMonitor)
     obsTempGen.createObservable(OBSERVATION_REFRESH_TIME).subscribe({
-        pub.publish("Dato Temperatura simulato " + it, obsTempGen.measuredParameter)
+        pub.publish("" + it, obsTempGen.measuredParameter)
     })
 
     val obsSysGen = ObservableMonitor<Double>(systolicMonitor)
     obsSysGen.createObservable(OBSERVATION_REFRESH_TIME).subscribe({
-        pub.publish("Dato Systolic blood pressure simulato " + it, obsSysGen.measuredParameter)
+        pub.publish("" + it, obsSysGen.measuredParameter)
     })
 
     val obsDiaGen = ObservableMonitor<Double>(diastolicMonitor)
     obsDiaGen.createObservable(OBSERVATION_REFRESH_TIME).subscribe({
-        pub.publish("Dato Diastolic blood pressure simulato " + it, obsDiaGen.measuredParameter)
+        pub.publish("" + it, obsDiaGen.measuredParameter)
     })
 
     val obsHrGen = ObservableMonitor<Double>(heartRateMonitor)
     obsHrGen.createObservable(OBSERVATION_REFRESH_TIME).subscribe({
-        pub.publish("Dato Heart rate simulato " + it, obsHrGen.measuredParameter)
+        pub.publish("" + it, obsHrGen.measuredParameter)
     })
 
     val obsSpO2Gen = ObservableMonitor(spO2Monitor)
     obsSpO2Gen.createObservable(OBSERVATION_REFRESH_TIME).subscribe({
-        pub.publish("Dato SpO2 simulato " + it, obsSpO2Gen.measuredParameter)
+        pub.publish("" + it, obsSpO2Gen.measuredParameter)
     })
 
     val obsEtCO2Gen = ObservableMonitor(etCO2)
     obsEtCO2Gen.createObservable(OBSERVATION_REFRESH_TIME).subscribe({
-        pub.publish("Dato EtCO2 simulato " + it, obsEtCO2Gen.measuredParameter)
+        pub.publish("" + it, obsEtCO2Gen.measuredParameter)
     })
 }
