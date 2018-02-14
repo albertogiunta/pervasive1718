e52450009994fbbcbf255b74a78cd10958e51761
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-02-14 18:12:59 +0100

    ADDED the SubjectController test in the micronotifier module

--- commons/src/main/kotlin/MicroserviceBootUtils.kt
+++ commons/src/main/kotlin/MicroserviceBootUtils.kt
@@ -11,10 +11,12 @@ fun Process.killAtParentDeath(): Process {
 }
 
 object MicroservicesPaths {
-    val microMonitor = System.getProperty("user.dir").dropLastWhile { it != '/' } + "micromonitor/build/libs/runMicroMonitor-0.1.jar"
-    val microDatabase = System.getProperty("user.dir").dropLastWhile { it != '/' } + "microdatabase/build/libs/runMicroDatabase-0.1.jar"
-    val microTask = System.getProperty("user.dir").dropLastWhile { it != '/' } + "microtask/build/libs/runMicroTask-0.1.jar"
-    val microNotifier = System.getProperty("user.dir").dropLastWhile { it != '/' } + "micronotifier/build/libs/runMicroNotifier-0.1.jar"
-    val microVisors = System.getProperty("user.dir").dropLastWhile { it != '/' } + "microvisors/build/libs/runMicroVisors-0.1.jar"
-    val microSession = System.getProperty("user.dir").dropLastWhile { it != '/' } + "microsession/build/libs/runMicroSession-0.1.jar"
+    private val separator = System.getProperty("file.separator").get(0)
+    private val internalPath = separator + "build" + separator + "libs" + separator
+    val microMonitor = System.getProperty("user.dir").dropLastWhile { it != separator } + "micromonitor" + internalPath + "runMicroMonitor-0.1.jar"
+    val microDatabase = System.getProperty("user.dir").dropLastWhile { it != separator } + "microdatabase" + internalPath + "runMicroDatabase-0.1.jar"
+    val microTask = System.getProperty("user.dir").dropLastWhile { it != separator } + "microtask" + internalPath + "runMicroTask-0.1.jar"
+    val microNotifier = System.getProperty("user.dir").dropLastWhile { it != separator } + "micronotifier" + internalPath + "runMicroNotifier-0.1.jar"
+    val microVisors = System.getProperty("user.dir").dropLastWhile { it != separator } + "microvisors" + internalPath + "runMicroVisors-0.1.jar"
+    val microSession = System.getProperty("user.dir").dropLastWhile { it != separator } + "microsession" + internalPath + "runMicroSession-0.1.jar"
 }
