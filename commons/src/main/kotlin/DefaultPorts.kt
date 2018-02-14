e7263b6bf2e3cb757cffb49a1b9e77bc479b0f98
michelecampobasso <michele.campobasso3@gmail.com>
2018-02-14 14:47:53 +0100

    ADDED default port to visors

--- commons/src/main/kotlin/DefaultPorts.kt
+++ commons/src/main/kotlin/DefaultPorts.kt
@@ -3,5 +3,6 @@ object DefaultPorts {
     const val dbPort = 8100
     const val taskPort = 8200
     const val clientSessionPort = 8300
+    const val visorsPort = 8400
     const val maxSimultaneousSessions = 10
 }
