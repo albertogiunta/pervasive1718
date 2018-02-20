e15616e8ae412e017bf79a9d3a3201eb74120649
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-02-20 17:19:14 +0100

    FIXED the MicroVisors webpage boot

--- microvisors/src/main/kotlin/main.kt
+++ microvisors/src/main/kotlin/main.kt
@@ -6,15 +6,25 @@ import utils.PathGetter
 import java.awt.Desktop
 import java.io.File
 import java.net.URI
+import java.net.URLEncoder
 
 fun main(args: Array<String>) {
     ConfigLoader().load(args)
 
     File(PathGetter.getRootPath()+"microvisors/src/view/js/portLoader.js").bufferedWriter().use { out -> out.write("var sessionExchange = " + Bootstrapper(Services.VISORS.port).port.toString()) }
     if (Desktop.isDesktopSupported()) {
-        val usrDir = PathGetter.getRootPath().replace("\\", "/").replace(" ", "%20")
+        var rootProjectPath = PathGetter.getRootPath()
+        if (System.getProperty("os.name").contains("Mac OS X")) {
+            rootProjectPath = URLEncoder.encode(rootProjectPath, "UTF-8").replace("%2F", "/").replace("+", " ")
+        } else if (System.getProperty("os.name").contains("Windows")) {
+            rootProjectPath = rootProjectPath.replace("\\", "/").replace(" ", "%20")
+        }
+        val usrDir = rootProjectPath.replace("\\", "/").replace(" ", "%20")
         val separator = "/"
-        Desktop.getDesktop().browse(URI("file:" + separator + separator + separator + usrDir + "microvisors" + separator + "src" + separator + "index.html"))
+        var path = "file:" + separator + separator + separator + usrDir + "microvisors" + separator + "src" + separator + "index.html"
+        var uri = URI(path)
+        println(uri)
+        Desktop.getDesktop().browse(uri)
     }
     RouteController.initRoutes()
 }
